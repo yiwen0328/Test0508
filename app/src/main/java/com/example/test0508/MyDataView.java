363309f04f42a613f57037c89d583688e5628957
CJCU <111B13910@mailst.cjcu.edu.tw>
2024-05-08 14:01:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/test0508/MyDataView.java
@@ -0,0 +1,14 @@
+package com.example.test0508;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyDataView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_data_view);
+    }
+}
